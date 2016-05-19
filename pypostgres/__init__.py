2a7ee5c7135ff09a6ab45dfb9d6a1341f8594a4d
Manoel Vilela <manoel_vilela@engineer.com>
2016-05-19 00:23:43 -0300

    Fix correct url of repo & pattern to download_link at github

--- pypostgres/__init__.py
+++ pypostgres/__init__.py
@@ -5,4 +5,4 @@
 __version__ = '0.1'
 __author__ = ['Marcellus Amadeus', 'Manoel Vilela']
 __email__ = ['marcellus@nexusedge.com.br', 'manoel.vilela@nexusedge.com.br']
-__url__ = 'https://github.com/marcelluzs/pypostgres/'
+__url__ = 'https://www.github.com/marcelluz/postgresql'
