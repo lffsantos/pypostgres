f44f81d49650bd13a908206b110db44c1215ebe7
Marcellus Amadeus <7marcellus@gmail.com>
2016-05-18 23:45:59 -0300

    Create pypostgres.py

---
+++ pypostgres/pypostgres.py
@@ -0,0 +1,45 @@
+import psycopg2 as pg
+import pandas as pd
+from itertools import repeat
+
+
+class Connection():
+
+    def __init__(self, db, user):
+        self.db = db
+        self.user = user
+
+    def __enter__(self, *args):
+        self.conn = pg.connect(
+            "dbname={} user={}".format(self.db, self.user))
+        self.cursor = self.conn.cursor()
+        return self.cursor
+
+    def __exit__(self, *args):
+        self.conn.commit()
+        self.cursor.close()
+        self.conn.close()
+
+
+class Postgres():
+
+    def __init__(self, db, user='postgres'):
+        self.db = db
+        self.user = user
+
+    def read(self, query):
+        with Connection(self.db, self.user) as cursor:
+            cursor.execute(query)
+            return cursor.fetchall()
+
+    def write(self, query, insertion=None):
+        with Connection(self.db, self.user) as cursor:
+            cursor.execute(query, insertion)
+
+    def to_dataframe(self, columns, table, conditions=None):
+        df = pd.DataFrame(columns=columns)
+        columns = ', '.join(df.columns)
+        result = self.read("SELECT {} FROM {};".format(columns, table))
+        for index, items in enumerate(result):
+            df.loc[index] = items
+        return df
