96823b95b7ce1fc508fd80d678122413e2cadae2
Marcellus Amadeus <void@marcellusamadeus.com>
2016-05-26 10:38:36 -0300

    add data frame building method

--- pypostgres/pypostgres.py
+++ pypostgres/pypostgres.py
@@ -6,35 +6,48 @@ from itertools import repeat
 
 from connection import Connection
 from utils import fix_int64
+from utils import untuple
 
 
 class Postgres():
 
-    def __init__(self):
-        pass
+    def __init__(self, table=None):
+        self.table = table
+        self.columns = get_table_columns(self.table) if table else None
 
-    def query(self, query, insertion=None, mode=['read', 'write']):
+    def query(self, query, values=None, result=False):
         with Connection() as session:
             connection, cursor = session
-            cursor.execute(query, insertion)
-            if mode == 'read':
+            cursor.execute(query, values)
+            if result:
                 return cursor.fetchall()
         return
 
-    def to_dataframe(self, columns, table, conditions=None):
+    @staticmethod
+    def get_table_columns(table):
+        sql = "select column_name from information_schema.columns where table_name='{}';"
+        columns = self.query(sql.format(table), result=True)
+        return untuple(columns)
+
+    @staticmethod
+    def build_dataframe(result_set, columns):
         df = pd.DataFrame(columns=columns)
-        columns = ', '.join(df.columns)
-
-        if conditions:
-            query = "SELECT {} FROM {} WHERE {};".format(
-                columns, table, conditions)
-        else:
-            query = "SELECT {} FROM {};".format(columns, table)
-
-        result = self.query(query, mode='read')
         for index, items in enumerate(result):
             df.loc[index] = items
         return df
+
+    def to_dataframe(self, table, columns=['all', list()], conditions=None):
+        flat_columns = ', '.join(columns)
+
+        if conditions:
+            query = "SELECT {} FROM {} WHERE {};".format(
+                flat_columns, table, conditions)
+        else:
+            query = "SELECT {} FROM {};".format(flat_columns, table)
+
+        result = self.query(query, result=True)
+        return self.build_dataframe(result, columns)
+        
 
     def from_dataframe(self, df, table):
         columns = ', '.join(df.columns)
@@ -45,4 +58,4 @@ class Postgres():
             # Numpy.int64 is not supported by psycopg2 type conversion
             # row first element is the DataFrame index
             values = [fix_int64(el) for el in row[1:]]
-            self.query(query, values, mode='write')
+            self.query(query, values)
