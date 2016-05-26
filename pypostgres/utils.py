d3c70bd54a798e89583318c05d5a07869fd92bb7
Marcellus Amadeus <void@marcellusamadeus.com>
2016-05-26 10:38:04 -0300

    add untuple func

--- pypostgres/utils.py
+++ pypostgres/utils.py
@@ -3,3 +3,8 @@ import numpy as np
 
 def fix_int64(data):
     return (data if not isinstance(data, np.int64) else int(data))
+
+
+def untuple(list_of_tuples, index=0):
+    for t in list_of_tuples:
+        yield t[index]
