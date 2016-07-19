f1680f9c55c494bdce56999cbf962a806bf2e4ff
Marcellus Amadeus <marcellus@nexusedge.com.br>
2016-07-19 18:07:11 -0300

    remove error description, add docstrings and add is nested func

--- pypostgres/utils.py
+++ pypostgres/utils.py
@@ -1,17 +1,25 @@
-import numpy as np
+#!/usr/bin/env python3
+#
+#   PyPostgres
+#   Marcellus Amadeus
+#
+
+# stdlib
 from collections import namedtuple
+
+# third-party
+import numpy as np
 
 
 Result = namedtuple('Result', ['success', 'response'])
-Error = namedtuple('Error', ['exception', 'name', 'description'])
+Error = namedtuple('Error', ['exception', 'name'])
 
 
-def fix_int64(data):
-    return (data if not isinstance(data, np.int64) else int(data))
+def fix_int64(value):
+    '''Replace numpy.int64 value for Python default int.'''
+    return value if not isinstance(value, np.int64) else int(value)
 
 
-def untuple(list_of_tuples, index=0):
-    result = []
-    for t in list_of_tuples:
-        result.append(t[index])
-    return result
+def is_nested(values):
+    '''Check whether given values have lists of lists (or tuples of tuples)'''
+    return all([(isinstance(el, list) or isinstance(el, tuple)) for el in values])
