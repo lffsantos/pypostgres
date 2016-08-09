7e0c12c326d103bd2e908fa3aaf5a857bf409e5e
Marcellus Amadeus <marcellus@nexusedge.com.br>
2016-08-09 15:08:33 -0300

    more fixes

--- pypostgres/postgres.py
+++ pypostgres/postgres.py
@@ -61,10 +61,11 @@ class Postgres(object):
                     # there is nothing to fetch
                     pass
             if data:
-                if is_nested(data) and all([len(row) == 1 for row in data]):
-                    data = [row[0] for row in data]
-                if len(data) == 1:
-                    data = data[0]
+                if hasattr(data, '__len__'):
+                    if is_nested(data) and all([len(row) == 1 for row in data]):
+                        data = [row[0] for row in data]
+                    elif len(data) == 1:
+                        data = data[0]
             return Result(True, data)
 
     def get_table_columns(self, table):
