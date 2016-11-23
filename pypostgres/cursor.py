199b0d7d00d5dd4fd60691dd374e17a5583ef29b
Marcellus Amadeus <marcellus@Marcelluss-MacBook-Air.local>
2016-11-23 11:12:08 -0200

    add smart default on Cursor for auto fetch when passing values

--- pypostgres/cursor.py
+++ pypostgres/cursor.py
@@ -4,14 +4,27 @@ from pypostgres.utils import is_nested
 
 
 class Cursor(object):
+    '''The Cursor object only holds the information about the query
+    only doing the actual request to db when fetching data except when
+    values are passed
+    '''
 
     def __init__(self, connection, sql, values, cursor_factory):
         self.connection = connection
         self.sql = sql
         self.values = values
         self.cursor_factory = cursor_factory
+        if self.values:
+            # passing values means insertion therefore
+            # there is no need to wait until fetch
+            self.fetch()
 
     def fetch(self, size=None):
+        '''Execute SQL statements given (alongside optional values) and fetch the result
+
+        :param size: fetch size, None (default) means there is not need to fetch anything
+        :type size: int or None (default)
+        '''
         with self.connection as conn:
             with conn.cursor(cursor_factory=self.cursor_factory) as cursor:
                 if self.values and is_nested(self.values):
