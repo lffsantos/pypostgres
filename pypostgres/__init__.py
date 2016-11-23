c83f084c6832c192b552c18c741179129e38d61a
Marcellus Amadeus <marcellus@Marcelluss-MacBook-Air.local>
2016-11-23 11:23:09 -0200

    update setup description and tags

--- pypostgres/__init__.py
+++ pypostgres/__init__.py
@@ -5,7 +5,7 @@
 #
 
 
-__version__ = '0.6.0'
+__version__ = '0.7.1'
 __author__ = ['Marcellus Amadeus', 'Manoel Vilela']
 __email__ = ['marcellus@nexusedge.com.br', 'manoel.vilela@nexusedge.com.br']
 __url__ = 'https://www.github.com/marcelluz/pypostgres'
