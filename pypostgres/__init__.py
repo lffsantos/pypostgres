524b9154a52eecdb1cfa0a63202ae0dbec8f4898
Marcellus Amadeus <marcellus@nexusedge.com.br>
2017-06-09 00:47:14 +0200

    skip v0.7.5

--- pypostgres/__init__.py
+++ pypostgres/__init__.py
@@ -6,7 +6,7 @@
 from pypostgres.postgres import Postgres
 from pypostgres.connection import Connection
 
-__version__ = '0.7.5'
+__version__ = '0.7.6'
 __author__ = ['Marcellus Amadeus', 'Manoel Vilela']
 __email__ = ['marcellus@nexusedge.co', 'manoel_vilela@engineer.com']
 __url__ = 'https://www.github.com/marcelluz/pypostgres'
