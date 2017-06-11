a16bf5c40a35f514a5cac1b71db6c6aeae7a937e
Marcellus Amadeus <marcellus@nexusedge.com.br>
2017-06-11 15:40:42 +0200

    fix setup install importerror

--- pypostgres/__init__.py
+++ pypostgres/__init__.py
@@ -3,10 +3,8 @@
 #   PyPostgres
 #   Marcellus Amadeus
 #
-from pypostgres.postgres import Postgres
-from pypostgres.connection import Connection
 
-__version__ = '0.7.6'
+__version__ = '0.7.7'
 __author__ = ['Marcellus Amadeus', 'Manoel Vilela']
 __email__ = ['marcellus@nexusedge.co', 'manoel_vilela@engineer.com']
 __url__ = 'https://www.github.com/marcelluz/pypostgres'
